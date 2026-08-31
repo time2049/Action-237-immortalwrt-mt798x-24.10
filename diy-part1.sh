2b61d5dd8776f584f5b46e651b5605903ddf7e70
time2049 <58364786+time2049@users.noreply.github.com>
2026-08-31 12:59:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,4 +10,8 @@
 # See /LICENSE for more information.
 #
 
-# 如需添加自定义软件源，可在下方添加 src-git 行
+# 1. 确保将默认的 feeds.conf.default 清理多余空行
+sed -i '/^$/d' feeds.conf.default
+
+# 2. 如果以后需要添加自定义源（如 kenzo/small 等），在此处取消注释并添加：
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
