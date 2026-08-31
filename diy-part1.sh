8202ef25eeb105190138bc8b230fdbda6ddbf64f
time2049 <58364786+time2049@users.noreply.github.com>
2026-08-31 21:33:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,24 @@
+```bash
 #!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# 1. 确保将默认的 feeds.conf.default 清理多余空行
-sed -i '/^$/d' feeds.conf.default
+set -e
 
-# 2. 如果以后需要添加自定义源（如 kenzo/small 等），在此处取消注释并添加：
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "========================================"
+echo "DIY Part 1"
+echo "========================================"
+
+# 清理 feeds.conf.default 多余空行
+if [ -f feeds.conf.default ]; then
+    sed -i '/^[[:space:]]*$/d' feeds.conf.default
+fi
+
+echo "feeds.conf.default:"
+cat feeds.conf.default
+
+echo "========================================"
+```
