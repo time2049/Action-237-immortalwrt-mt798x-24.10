c3a7df160eb404fec5ea1c2594a449cc77a05d87
time2049 <58364786+time2049@users.noreply.github.com>
2026-09-04 15:45:07 +0800

    Refactor diy-part1.sh for OpenWrt customization

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
-set -e
+# 修改默认 IP 为 192.168.6.1
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-echo "========================================"
-echo "DIY Part 1: Start"
-echo "========================================"
+# 修改主机名 (去除 PW 标识)
+sed -i 's/ImmortalWrt/ImmortalWrt-24.10-6.6-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
 
-# 清理 Windows 换行
-sed -i 's/\r$//' feeds.conf.default 2>/dev/null || true
-
-# 删除空行
-if [ -f feeds.conf.default ]; then
-    sed -i '/^[[:space:]]*$/d' feeds.conf.default
-fi
-
-# ==========================================================
-# 删除不需要的 telephony feed
-# 360T7 纯净版不需要电话/VoIP软件
-# ==========================================================
-
-sed -i '/^[[:space:]]*src-git[[:space:]]\+telephony[[:space:]]/d' \
-    feeds.conf.default
-
-sed -i '/^[[:space:]]*src-git-full[[:space:]]\+telephony[[:space:]]/d' \
-    feeds.conf.default
-
-echo "========== feeds.conf.default =========="
-
-cat feeds.conf.default
-
-echo "========================================"
-echo "DIY Part 1: Completed"
-echo "========================================"
+# 修改固件输出文件名，添加日期前缀 (去除 PW 标识)
+sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-24.10-6.6-|' include/image.mk
