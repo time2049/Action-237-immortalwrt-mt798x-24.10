09ff51f0d3eaa06e1615d1b6651bb21d7db15796
time2049 <58364786+time2049@users.noreply.github.com>
2026-09-04 15:45:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,71 +1,20 @@
 #!/bin/bash
 #
-# 360 T7 ImmortalWrt 24.10 / Kernel 6.6
-# DIY Part 2
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
 #
 
-set -e
+# 修改默认 IP 为 192.168.6.1
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-echo "========================================"
-echo "DIY Part 2: Start"
-echo "========================================"
+# 修改主机名 (去除 PW 标识)
+sed -i 's/ImmortalWrt/ImmortalWrt-24.10-6.6-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
 
-# ==========================================================
-# 1. 默认 LAN IP
-# ==========================================================
-
-CONFIG_GENERATE="package/base-files/files/bin/config_generate"
-
-if [ -f "$CONFIG_GENERATE" ]; then
-    sed -i 's/192\.168\.1\.1/192.168.6.1/g' "$CONFIG_GENERATE"
-fi
-
-# ==========================================================
-# 2. 默认主机名
-# ==========================================================
-
-CURRENT_DATE=$(TZ="Asia/Shanghai" date +"%Y%m%d")
-
-if [ -f "$CONFIG_GENERATE" ]; then
-    sed -i \
-        "s/ImmortalWrt/ImmortalWrt-24.10-6.6-${CURRENT_DATE}/g" \
-        "$CONFIG_GENERATE"
-fi
-
-# ==========================================================
-# 3. 固件文件名前缀
-# ==========================================================
-
-if [ -f include/image.mk ]; then
-    sed -i \
-        's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-24.10-6.6-|' \
-        include/image.mk
-fi
-
-# ==========================================================
-# 4. 删除不需要的软件包源码
-# ==========================================================
-
-echo "========== 删除不需要的软件包 =========="
-
-# PassWall
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-passwall2
-rm -rf feeds/packages/net/passwall
-
-# 代理核心
-rm -rf feeds/packages/net/sing-box
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/v2ray-core
-
-# DDNS-Go
-rm -rf feeds/packages/net/ddns-go
-rm -rf feeds/luci/applications/luci-app-ddns-go
-
-# VLMCSd
-rm -rf feeds/packages/net/vlmcsd
-rm -rf feeds/luci/applications/luci-app-vlmcsd
-
-echo "========================================"
-echo "DIY Part 2: Completed"
-echo "========================================"
+# 修改固件输出文件名，添加日期前缀 (去除 PW 标识)
+sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-24.10-6.6-|' include/image.mk
