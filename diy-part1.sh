08bea4a5aa2a5daba9f438d51e066b5726f495a6
time2049 <58364786+time2049@users.noreply.github.com>
2026-09-06 23:48:12 +0800

    Update diy-part1.sh with feed source instructions
    Added comments for adding custom software sources.

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,4 +10,10 @@
 # See /LICENSE for more information.
 #
 
-# 如需添加自定义软件源，可在下方添加 src-git 行
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
