9c10e22c614bfeed8716ff9d94a3ff20bf2254da
time2049 <58364786+time2049@users.noreply.github.com>
2026-09-06 22:27:28 +0800

    Fix compilation errors in Mediatek driver
    Add missing macro definitions for Mediatek Ethernet driver.

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,3 +29,13 @@ package/base-files/files/bin/config_generate
 # ----------------------------------------------------------
 sed -i "s|IMG_PREFIX:=|IMG_PREFIX:=${HOST_DATE}-24.10-6.6-|" \
 include/image.mk
+
+
+# ----------------------------------------------------------
+# 自动修复 Kernel 6.6 联发科网卡驱动缺失宏定义的编译错误
+# ----------------------------------------------------------
+find target/linux/mediatek/ -name "mtk_eth_soc.h" -exec sed -i '/#define __MTK_ETH_SOC_H/a \
+#ifndef MTK_FE_RESET_DONE\n#define MTK_FE_RESET_DONE 0x01\n#endif\n\
+#ifndef MTK_WIFI_RESET_DONE\n#define MTK_WIFI_RESET_DONE 0x02\n#endif\n\
+#ifndef MTK_WIFI_CHIP_ONLINE\n#define MTK_WIFI_CHIP_ONLINE 0x03\n#endif\n\
+#ifndef MTK_WIFI_CHIP_OFFLINE\n#define MTK_WIFI_CHIP_OFFLINE 0x04\n#endif' {} +
