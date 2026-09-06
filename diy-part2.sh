ed7b1d42eaf0a948dd8fad113ab31487e996adbb
time2049 <58364786+time2049@users.noreply.github.com>
2026-09-06 23:49:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,40 +2,25 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
 #
 
-# ==========================================================
-# 360 T7 / ImmortalWrt 24.10 / Kernel 6.6
-# ==========================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# ----------------------------------------------------------
-# 默认 LAN IP
-# ----------------------------------------------------------
-sed -i 's/192.168.1.1/192.168.6.1/g' \
-package/base-files/files/bin/config_generate
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+# Modify hostname
+sed -i 's/ImmortalWrt/ImmortalWrt-24.10-6.6-PW-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
 
-# ----------------------------------------------------------
-# 主机名
-# ----------------------------------------------------------
-HOST_DATE=$(TZ="Asia/Shanghai" date +"%Y%m%d")
+# Modify filename, add date prefix
+sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW-24.10-6.6-|' include/image.mk
 
-sed -i "s/ImmortalWrt/ImmortalWrt-24.10-6.6-${HOST_DATE}/g" \
-package/base-files/files/bin/config_generate
-
-
-# ----------------------------------------------------------
-# 固件输出文件名
-# ----------------------------------------------------------
-sed -i "s|IMG_PREFIX:=|IMG_PREFIX:=${HOST_DATE}-24.10-6.6-|" \
-include/image.mk
-
-
-# ----------------------------------------------------------
-# 自动修复 Kernel 6.6 联发科网卡驱动缺失宏定义的编译错误
-# ----------------------------------------------------------
-find target/linux/mediatek/ -name "mtk_eth_soc.h" -exec sed -i '/#define __MTK_ETH_SOC_H/a \
-#ifndef MTK_FE_RESET_DONE\n#define MTK_FE_RESET_DONE 0x01\n#endif\n\
-#ifndef MTK_WIFI_RESET_DONE\n#define MTK_WIFI_RESET_DONE 0x02\n#endif\n\
-#ifndef MTK_WIFI_CHIP_ONLINE\n#define MTK_WIFI_CHIP_ONLINE 0x03\n#endif\n\
-#ifndef MTK_WIFI_CHIP_OFFLINE\n#define MTK_WIFI_CHIP_OFFLINE 0x04\n#endif' {} +
+# Modify ppp-down, add sleep 3. 2025-6-13 source code is update, no need this
+#sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
