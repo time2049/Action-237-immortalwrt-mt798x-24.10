e11ac78ab513befd0cec525b591b4ce98a21a35d
time2049 <58364786+time2049@users.noreply.github.com>
2026-09-07 00:30:08 +0800

    Update hostname and filename in diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,11 +16,11 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-sed -i 's/ImmortalWrt/ImmortalWrt-24.10-6.6-PW-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
+# Modify hostname (移除 "PW"，改为 "360T7")
+sed -i 's/ImmortalWrt/ImmortalWrt-24.10-6.6-360T7-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
 
-# Modify filename, add date prefix
-sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW-24.10-6.6-|' include/image.mk
+# Modify filename, add date prefix (移除 "PW"，改为 "360T7")
+sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-360T7-24.10-6.6-|' include/image.mk
 
 # Modify ppp-down, add sleep 3. 2025-6-13 source code is update, no need this
 #sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
