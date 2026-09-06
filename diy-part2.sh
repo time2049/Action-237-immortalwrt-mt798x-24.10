f999669a25716ec4003243e8ff6c63445a55815a
time2049 <58364786+time2049@users.noreply.github.com>
2026-09-06 22:20:20 +0800

    Update OpenWrt DIY script for hostname and IP changes

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,19 +2,30 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
 #
 
-# 修改默认 IP 为 192.168.6.1
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+# ==========================================================
+# 360 T7 / ImmortalWrt 24.10 / Kernel 6.6
+# ==========================================================
 
-# 修改主机名 (去除 PW 标识)
-sed -i 's/ImmortalWrt/ImmortalWrt-24.10-6.6-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
+# ----------------------------------------------------------
+# 默认 LAN IP
+# ----------------------------------------------------------
+sed -i 's/192.168.1.1/192.168.6.1/g' \
+package/base-files/files/bin/config_generate
 
-# 修改固件输出文件名，添加日期前缀 (去除 PW 标识)
-sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-24.10-6.6-|' include/image.mk
+
+# ----------------------------------------------------------
+# 主机名
+# ----------------------------------------------------------
+HOST_DATE=$(TZ="Asia/Shanghai" date +"%Y%m%d")
+
+sed -i "s/ImmortalWrt/ImmortalWrt-24.10-6.6-${HOST_DATE}/g" \
+package/base-files/files/bin/config_generate
+
+
+# ----------------------------------------------------------
+# 固件输出文件名
+# ----------------------------------------------------------
+sed -i "s|IMG_PREFIX:=|IMG_PREFIX:=${HOST_DATE}-24.10-6.6-|" \
+include/image.mk
